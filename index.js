51a238e1f65583fd652cb4cf2c3990f02e45fbcd
“Veronica <vero.parragraca@gmail.com>
2024-02-09 22:12:42 +0100

    resueltos hasta bucles(punto 10

---
+++ index.js
@@ -0,0 +1,21 @@
+/* BIKRAM JS*/
+//Varibles
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI * 2;
+
+//Booleanos
+let booleanoAnd = booleano1 === booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 =
+  (booleano1 || booleano2) === (booleano1 || (!booleano1 && !booleano2));
+
+//Operadores
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+
+//Bucles
